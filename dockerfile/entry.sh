5bdd0f76b0829bbcf4ab0e932a62eefe4bdf0468
MrLYC <imyikong@gmail.com>
2016-10-13 17:35:59 +0800

    add cleaner

--- dockerfile/entry.sh
+++ dockerfile/entry.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+function cleaner() {
+    while true; do
+        sleep 60
+        find /site/download/ -mmin +${FILE_EXPIRES} -type f -exec rm -f {} \;
+    done
+}
+
 nginx -t
 /etc/init.d/nginx start
+cleaner &
 aria2c --conf-path=/etc/aria2.conf --rpc-user=${RPC_USER} --rpc-passwd=${RPC_PASSWD}
